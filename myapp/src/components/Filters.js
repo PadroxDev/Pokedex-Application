bba2b992766dc60aa37b68e1f90b956d8676a37c
Padrox <antoinevollet.pro@gmail.com>
2022-12-13 11:56:26 +0100

    All good

--- myapp/src/components/Filters.js
+++ myapp/src/components/Filters.js
@@ -4,12 +4,56 @@ import { useForm } from 'react-hook-form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+Array.prototype.alphaSort = function (data) {
+    function compareAlphabetical(a, b) {
+        return 1;
+    }
+    
+    function compareAlphabeticalInverse(a, b) {
+        return 1;
+    }
+    
+    function compareNumberAscending(a, b) {
+        if (a.number < b.number)
+            return -1;
+        if (a.number > b.number)
+            return 1;
+        return 0;
+    }
+    
+    function compareNumberDescending(a, b) {
+        if (a.number > b.number)
+            return -1;
+        if (a.number < b.number)
+            return 1;
+        return 0;
+    }
+}
+
+
+function compare(data, a, b) {
+    switch(data.sortOrder) {
+        case "Alphabetical ↓":
+            return compareAlphabetical(a, b);
+        case "Alphabetical ↑":
+            return compareAlphabeticalInverse(a, b);
+        case "Number ↑":
+            return compareNumberAscending(a, b);
+        case "Numéro ↓":
+            return compareNumberDescending(a, b);
+    }
+}
+
 function Filters(props) {
     const { register, handleSubmit } = useForm();
     const onSubmit = (data) => {
-        console.log(data);
+        if (data.primaryType === "All") {
+            props.setPokemonsShow(props.pokemons.sort(compare(data)));
+        } else {
+            props.setPokemonsShow(props.pokemons.filter(compare(data)));
+        }
     }
-
+ 
     function GenerateTypesOptions(props) {
         return props.types.map((type, key) => (
             <option key={key} value={type.name}>{type.name}</option>
@@ -20,33 +64,28 @@ function Filters(props) {
 
     return (
         <Form className="filters-form" onSubmit={handleSubmit(onSubmit)}>
-            <h1>UWU</h1>
-            <h1>UWU</h1>
-            <h1>UWU</h1>
-            <h1>UWU</h1>
-            <h1>UWU</h1>
             <Row>
-                <Col size={4}>
+                <Col>
                     <Form.Group className="mb-3" controlId="filterPrimaryType">
-                        <Form.Label>Primary Type</Form.Label>
+                        <Form.Label>Type principal</Form.Label>
                         <Form.Select {...register("primaryType")}>
-                            <option value="All">All</option>
+                            <option value="All">Tous</option>
                             {dropdownOptions}
                         </Form.Select>
                     </Form.Group>
                 </Col>
                 
-                <Col size={4}>
+                {/* <Col>
                     <Form.Group className="mb-3" controlId="filterSecondaryType">
-                        <Form.Label>Secondary Type</Form.Label>
+                        <Form.Label>Type Secondaire</Form.Label>
                         <Form.Select {...register("secondaryType")}>
-                            <option value="None">None</option>
+                            <option value="None">Aucun</option>
                             {dropdownOptions}
                         </Form.Select>
                     </Form.Group>
-                </Col>
+                </Col> */}
 
-                <Col size={4}>
+                <Col>
                     <Form.Group className="mb-3" controlId="filterSortOrder">
                         <Form.Label>Sort Order</Form.Label>
                         <Form.Select {...register("sortOrder")}>
@@ -57,7 +96,7 @@ function Filters(props) {
                         </Form.Select>
                     </Form.Group>
                 </Col>
-                <Col className="filters-button-column" size={1}>
+                <Col className="filters-button-column">
                     <Form.Group className="mb-3" controlId="filterSubmitButton">
                         <Button variant="success" type="submit">
                             Filtrer
