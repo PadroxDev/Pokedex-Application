83069e3e82f271485402b067e6bfc359209c05a7
William Bailleul <wbailleul@gaming.tech>
2022-12-14 01:36:32 +0100

    tentative de refaire ce que j'avais fait

--- myapp/src/components/ListExample.js
+++ myapp/src/components/ListExample.js
@@ -4,14 +4,16 @@ import "../App.css";
 function ListExample() {
   return (
     <div className="follow-scroll-navbar">
-      <Nav className="navbar color-red" defaultActiveKey="/home" as="ul">
-        <img className="home-button-image color-red" src="./img/home_button.png"/>     
-        <Nav.Item as="li">
-          <Nav.Link href="/pokemons" eventKey="link-1 white-text">Pokémon</Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link href="/pokedex" eventKey="link-2 white-text">Pokédex</Nav.Link>
-        </Nav.Item>
+      <Nav className="color-red" defaultActiveKey="/home" as="ul">
+        <img className="home-button-image" src="./img/home_button.png"/>     
+        <div className='navbar-text-link'>
+          <Nav.Item as="li">
+            <Nav.Link className="navbar-items" href="/pokemons" eventKey="link-1 white-text">Pokémon</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link className="navbar-items" href="/pokedex" eventKey="link-2 white-text">Pokédex</Nav.Link>
+          </Nav.Item>
+        </div>
       </Nav>
       <div className="pokedex-style-gradient row">
         <div className='pokedex-top-part-shape'></div>
