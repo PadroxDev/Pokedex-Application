da4d573e827bc8b153bf60f292daf8ae769af2c3
PadroxDev <antoinevolletfr@gmail.com>
2022-12-15 07:07:54 +0100

    Final Changes

--- myapp/src/components/StarterPicker.js
+++ myapp/src/components/StarterPicker.js
@@ -1,33 +1,34 @@
 import "../App.css";
 import { useState, useEffect } from 'react';
 import { getAllPokemons ,getPokemonByNumber} from "../api/pokemons";
-import { getAllTypes } from "../api/types"
+import { getAllTypes } from "../api/types";
+import { updatePokedex } from "../api/pokedex";
+import { getPokemonByName } from "../api/pokemons";
 
 function StarterPicker(props) {
-    const [concernedStarter,setConcernedStarter]= useState(null);
-    const starters=["001","004","007"];
-    const getStarter=async(index)=>{
-        return await getPokemonByNumber(starters[index])
-            .then(result => setConcernedStarter(result))
-            .catch(err => console.log(err));
+    let alreadySelected = false;
+
+    const shinyRate = 10;
+
+    const getRandomInt = (max) => {
+        return Math.floor(Math.random() * max);
+    }
+
+    const handleClick = async (e, pokemon) => {
+        if (alreadySelected) return;
+
+        alreadySelected = true;
+
+        const fetchedPokemon = await getPokemonByName(pokemon);
+
+        const isShiny = getRandomInt(shinyRate) == 0;
+        updatePokedex(pokemon, {name:fetchedPokemon.name, number:fetchedPokemon.number, types:fetchedPokemon.types, imgUrl:isShiny ? fetchedPokemon.imgUrlShiny : fetchedPokemon.imgUrl, shiny:isShiny});
     }
 
     return <div className='starter-choice' >
-        {/* {
-            starters.map((v,key)=>{
-                getStarter(v);
-                console.log(getStarter(v))
-                if (!concernedStarter) return;
-                return <div 
-                    key={key}className='starter'>
-                        <img src={concernedStarter.imgUrl}/>
-                </div>
-            })
-
-        } */}
-        <div className="starter"><img src = "../img/Bulbizarre.png" alt="Bulbizarre"/></div>
-        <div className="starter"><img src = "../img/Salamèche.png" alt="Salamèche"/></div>
-        <div className="starter"><img src = "../img/Carapuce.png" alt="Carapuce"/></div>
+        <div className="starter" onClick={e => handleClick(e, "Bulbizarre")}><img src = "../img/Bulbizarre.png" alt="Bulbizarre"/></div>
+        <div className="starter" onClick={e => handleClick(e, "Salamèche")}><img src = "../img/Salamèche.png" alt="Salamèche"/></div>
+        <div className="starter" onClick={e => handleClick(e, "Carapuce")}><img src = "../img/Carapuce.png" alt="Carapuce"/></div>
     </div>
 }
 
